1dab2b46ba34c8215afbd787485029174db1ef90
Mike McPherson <mmcp42@yahoo.com>
2014-01-14 12:12:09 +0000

    settings file, changed baud rate
    global settings now an include file
    baud rate increased to 57600

--- xrfTest.ino
+++ xrfTest.ino
@@ -25,19 +25,9 @@ DateTime timeNow;
 #define BATVOLT_R2      2               // voltage divider R2 = 2M
 #define ADC_AREF        3.3
 
-//==============================================================
-// "globals"
-//==============================================================
-uint8_t      gAPN[32] = "APN";                            // APN from your service provider
-uint8_t   gServer[16] = "ftpServer";                      // URL of your FTP server
-uint8_t gUserName[16] = "username";                       // username for ftp connect
-uint8_t gPassword[16] = "password";                       // password for ftp connect
-uint8_t  gFTPpath[16] = "/ftpPath/";                      // path to location of ftp files
-uint8_t     gWxId[16] = "wxID";                           // id of your weather station
-uint8_t    gMagic[ 8] = "wxftp";                          // to identify valid records
-uint8_t   gFtpSql[48] = "http://www..../wxftp2sql.php";   // URL of php script to read ftp and write to database
-uint8_t  gTimeURL[48] = "http://www..../ts.php";          // URL that returns Unix epoch
-
+// get global settings
+//====================
+#include "globals.h"
 
 boolean gLoop;
 
@@ -53,7 +43,7 @@ void setup(void)
   flashInit();
   
   digitalWrite(XRF, LOW);
-  Serial.begin(9600);
+  Serial.begin(57600);
   showVersion();
   showCommands();
   gLoop = false;
